ba0631e3c6de24dd079355c9c7552ac1ac4c7baa
David W <daveo1001@gmail.com>
2019-06-22 01:53:30 -0500

    fuller pages. and flex-layout!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
+import { LayoutModule } from '@angular/cdk/layout';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatListModule, MatSidenavModule, MatToolbarModule, MatSelectModule, MatDatepickerModule, MatNativeDateModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { NavigationComponent } from './navigation/navigation.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { LandingComponent } from './landing/landing.component';
-import { RegistrationComponent } from './registration/registration.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LandingComponent } from './landing/landing.component';
+import { NavigationComponent } from './navigation/navigation.component';
 import { ProfileComponent } from './profile/profile.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
@@ -28,10 +29,17 @@ import { ProfileComponent } from './profile/profile.component';
     MatButtonModule,
     HttpClientModule,
     LayoutModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
     MatToolbarModule,
     MatSidenavModule,
+    MatSelectModule,
     MatIconModule,
     MatListModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
+  public linesOfService = [
+    { name: 'BA' },
+    { name: 'D&A' },
+    { name: 'SA&E' },
+  ];
+
+  public branches = [
+    { name: 'STL'},
+    { name: 'MSL'}
+  ];
 
   constructor() { }
 
