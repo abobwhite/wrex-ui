6eb28dcbda760c5b6facd0e78e3b0fa7b1c47348
David W <daveo1001@gmail.com>
2019-06-22 15:14:23 -0500

    local proxy and new prod env.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
+const apiBase = '/api';
+
 export const environment = {
   production: true,
   apiEndpoints: {
-    getRecommendations: `/users/:userId/recommendations`,
-    patchRecommendation: `/users/:userId/recommendations/:recommendationId`
+    getRecommendations: `${apiBase}/users/:userId/recommendations`,
+    patchRecommendation: `${apiBase}/users/:userId/recommendations/:recommendationId`
   }
 };
