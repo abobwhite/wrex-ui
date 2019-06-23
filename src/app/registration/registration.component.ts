c4c76bd0185fe4f0975ee3b95cb99d1cdefa9cad
David W <daveo1001@gmail.com>
2019-06-23 02:52:07 -0500

    happy query param getting

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core'
-import { ActivatedRoute } from '@angular/router'
-import { UserService } from '../services/user.service'
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ReferenceService } from '../services/reference.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-registration',
@@ -8,28 +9,45 @@ import { UserService } from '../services/user.service'
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
-  public linesOfService = [
-    { name: 'BA' },
-    { name: 'D&A' },
-    { name: 'SA&E' },
-  ]
+  public linesOfService;
+  public branches;
 
-  public branches = [
-    { name: 'STL' },
-    { name: 'MSL' }
-  ]
+  public linesOfServiceLoading = true;
+  public branchesLoading = true;
 
-  constructor(private activatedRoute: ActivatedRoute, private userService: UserService) {
+  constructor(private activatedRoute: ActivatedRoute, private userService: UserService, private referenceService: ReferenceService) {
   }
 
   ngOnInit() {
-    const code = this.activatedRoute.snapshot.paramMap.get('code');
+    this.manageSlackCode();
+    this.getBranches();
+    this.getLinesOfService();
+  }
+
+  private manageSlackCode() {
+    const code = this.activatedRoute.snapshot.queryParamMap.get('code');
     if (code) {
       this.userService.postUsersCode(code).subscribe((thing) => {
         console.log(thing);
+      }, (e) => {
+        console.log(e, 'error posting slack code');
       })
     } else {
       console.log('no code, bruh')
     }
+  }
+
+  private getBranches() {
+    this.referenceService.getBranches().subscribe((branches) => {
+      this.branches = branches
+      this.branchesLoading = false;
+    })
+  }
+
+  private getLinesOfService() {
+    this.referenceService.getLinesOfService().subscribe((linesOfService) => {
+      this.linesOfService = linesOfService;
+      this.linesOfServiceLoading = false;
+    })
   }
 }
